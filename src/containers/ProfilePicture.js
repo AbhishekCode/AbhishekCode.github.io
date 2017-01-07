2fb00e63d5cf8b32c3d10a0854822ae6e0a2bf0d
Abhishek Singh <abhishek@tekhne.nl>
2017-01-07 19:30:26 +0530

    Update

---
+++ src/containers/ProfilePicture.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { StyleSheet, css } from 'aphrodite';
+
+const profilePicUrl = 'https://pbs.twimg.com/profile_images/758268303496376320/2hxvfwrG_bigger.jpg';
+const imageSize = 200;
+const borderSize = 5;
+
+export default class ProfilePicture extends Component {
+  render() {
+    return (
+        <div className={css(styles.container)}>
+            <div className={css(styles.imageContainer)}>
+                <img src={profilePicUrl} height={imageSize} width={imageSize} alt="profile"/>
+            </div>
+        </div>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+    container: {
+       display: 'flex',
+       flexDirection: 'column',
+       justifyContent: 'center',
+       alignItems: 'center',
+       zIndex:2,
+       padding: 20
+    },
+    imageContainer:{
+       display: 'flex',
+       marginTop: 50,
+       height: imageSize,
+       width:imageSize,
+       borderRadius: borderSize+ imageSize/2,
+       overflow: 'hidden',
+       borderWidth: borderSize,
+       borderColor: 'white',
+       borderStyle: 'solid'
+    }
+});
+
